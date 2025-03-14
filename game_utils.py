67dd8d6dc24e71e46d5f6b9ada20513d8fe2e637
Aaron <aaronjmatayka@gmail.com>
2025-03-14 01:21:30 -0500

    Added in check_collision function for checking if two players occupy the same spot, and modified the loop to use this function to end the program if there is a collision, currently used for testing. Added in TODO for all current missing features as we currently know them.

--- game_utils.py
+++ game_utils.py
@@ -49,8 +49,17 @@ def place_players(grid, players=2, positions=None):
 
     return player_objects
 
+#Function used to check if two players are on the same spot, and if so print this and return true.
+def check_collision(player):
+    player_positions = set()
+    for player in players:
+        if player.position in player_positions:
+            print("Collision detected!")
+            return True #Collision detected
+        player_positions.add(player.position)
+    print("No collision detected!")
+    return False #No collision
 # Testing code
-# TODO: move to main
 test_grid = make_grid(5, 3)
 print_grid(test_grid)
 
@@ -61,5 +70,25 @@ print_grid(test_grid)
 for _ in range(25):  # Move 10 times
     if players:
         players[0].move("random", test_grid)
+        players[1].move("random", test_grid)
     print_grid(test_grid)
+
+    if check_collision(players):
+        print("Players have met! Stopping simulation.")
+        break
     time.sleep(0.5)  # Pause for half a second to visualize changes
+
+
+# TODO: move test code at bottom to main
+# TODO: Make 3 different py files, one for each scenario we have to implement: K-2, 3-5, and 6-8
+# TODO: For each scenario, music needs to play during gameplay.
+# TODO: For each scenario, when all players are in a group display a graphic and statistics from wandering, and announe them audibly. Game resets.
+# TODO: For K-2, implement 2 people in opposite corners, who wander randomly. Count each move for each person. Basically the default scenario for functions.
+# TODO: For 3-5, grid size can be set manually, which can now be rectangular. 2, 3 or 4 people, can be placed anywhere. Can play and replay multiple times, and display stats like longest run, shortest, average etc.
+# TODO: For 6-8, Students are meant to run experiments to find how average runs vary with different sizes and shapes of grids, will need run-to-run tracking like 3-5. Also need different wandering protocols to use besides random.
+# TODO: Change the appearance of two collided players to better represent they are both there, currently just 1 or 2 appears.
+# TODO: Implement main to run in a loop for testing multiple times and multiple levels of complexity, needs to run until user ends it.
+# TODO: Design document: Needs both specifications and software design, sample document is posted. Can be combined with user's guide.
+# TODO: User's guide, simple instructions to tell users how to install and use the program.
+# TODO: Finished code needs to be in exe file or binary, click to execute.
+# TODO: Demo of code, given 20 minutes to demo our code.
